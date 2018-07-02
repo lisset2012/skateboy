08453cd78614a2ef2c3ec829f578a980ebf57657
lisset2012 <lisset2012@gmail.com>
2018-07-02 18:11:56 -0400

    Still jumping at the end

--- main.js
+++ main.js
@@ -1,60 +1,58 @@
 window.onscroll = function (e) {
-  if (document.body.scrollTop < 50) {
+  // console.log(e)
+  if (document.body.scrollTop < 50 || document.documentElement.scrollTop < 50) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '0px 0px';
-    document.getElementById("crazy_boy").style.top = '50%';
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top = '-80px';
+    console.log(document.documentElement.scrollTop);
   }
 
 
-  if (document.body.scrollTop > 50 && document.body.scrollTop < 100) {
+  if (document.body.scrollTop > 50 &&  document.body.scrollTop < 400 || document.documentElement.scrollTop> 50 &&  document.documentElement.scrollTop < 400) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-72px 0px';
-    document.getElementById("crazy_boy").style.top ='50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top ='50%'; 
   }
 
-  if (document.body.scrollTop > 100 && document.body.scrollTop < 200) {
+  if (document.body.scrollTop > 400 && document.body.scrollTop < 800 || document.documentElement.scrollTop> 400 &&  document.documentElement.scrollTop < 800) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-144px 0px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top = '50%'; 
   }
 
-  if (document.body.scrollTop > 200 && document.body.scrollTop < 1983) {
+  if (document.body.scrollTop > 800 && document.body.scrollTop < 1200 ||document.documentElement.scrollTop> 800 &&  document.documentElement.scrollTop < 1200) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '0px -80px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
+    document.getElementById("crazy_boy").style.top = '50%'; 
     console.log(document.body.scrollTop);
   }
 
-  if (document.body.scrollTop > 1983 && document.body.scrollTop < 2644) {
+  if (document.body.scrollTop > 1200 && document.body.scrollTop < 1800 || document.documentElement.scrollTop> 1200 &&  document.documentElement.scrollTop < 1800) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-72px -80px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top = '50%'; 
+    
   }
 
-  if (document.body.scrollTop > 2644 && document.body.scrollTop < 3304) {
+  if (document.body.scrollTop > 1800 && document.body.scrollTop < 2200 || document.documentElement.scrollTop> 1800 &&  document.documentElement.scrollTop < 2200) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-144px -80px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top = '50%'; 
+    
   }
 
-  if (document.body.scrollTop > 3304 && document.body.scrollTop < 3966) {
+  if (document.body.scrollTop > 2200 && document.body.scrollTop < 3000 || document.documentElement.scrollTop> 2200 &&  document.documentElement.scrollTop < 3000) {
 
-    document.getElementById("crazy_boy").style.backgroundPosition = '-0px -160px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.backgroundPosition = '0px -160px';
+    document.getElementById("crazy_boy").style.top = '50%'; 
+    
   }
 
-  if (document.body.scrollTop > 3966 && document.body.scrollTop < 4675) {
+  if (document.body.scrollTop > 3000 && document.body.scrollTop < 3500 || document.documentElement.scrollTop> 3000 &&  document.documentElement.scrollTop < 3500) {
 
     document.getElementById("crazy_boy").style.backgroundPosition = '-72px -160px';
-    document.getElementById("crazy_boy").style.top = '50%'; //window.innerHeight / 2 + 'px';//document.getElementById("crazy_boy").offsetHeight / 2
-    console.log(document.body.scrollTop);
+    document.getElementById("crazy_boy").style.top = '50%'; 
+   
   }
-  // console.log('scroll is here ===>' + document.body.scrollTop);
-  // add the rest of the conditions here
+  
 }
